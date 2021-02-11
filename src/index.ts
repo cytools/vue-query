631e2d5a3d52656a76c7814771d13a1576794730
Kaloyan Yosifov <kaloqn665@gmail.com>
2021-02-11 20:55:56 +0200

    Prepare for publishing

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies.
+ */
+import useQuery from '@/vue/query/useQuery';
+import useMutation from '@/vue/useMutation';
+import useQueryClient from '@/vue/useQueryClient';
+
+export {
+    useQuery,
+    useMutation,
+    useQueryClient,
+};
